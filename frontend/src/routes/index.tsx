6019a16de9ed2d8c5a45f7b0f8e2fb4df67a6f4b
Krystian Olechowski <krystian@Air-Krystian.lan>
2025-01-08 21:23:08 +0100

    add google login

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -45,6 +45,27 @@ function IndexRoute() {
           count is {count}
         </button>
         <Button variant="outline">Button</Button>
+        <div
+          id="g_id_onload"
+          data-client_id="<client ID>"
+          data-context="signin"
+          data-ux_mode="popup"
+          data-callback="handleSignInWithGoogle"
+          data-nonce=""
+          data-auto_select="true"
+          data-itp_support="true"
+          data-use_fedcm_for_prompt="true"
+        ></div>
+
+        <div
+          className="g_id_signin"
+          data-type="standard"
+          data-shape="pill"
+          data-theme="outline"
+          data-text="signin_with"
+          data-size="large"
+          data-logo_alignment="left"
+        ></div>
       </div>
     </>
   );
